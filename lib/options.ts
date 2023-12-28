f7f190ceb523504956065959436fa57f4d60f4f0
Stephen Lewis <gpg@stephenlewis.me>
2023-12-28 17:48:45 +0000

    Define default options as separate object

--- lib/options.ts
+++ lib/options.ts
@@ -1,8 +1,10 @@
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 
+const defaults = { radius: 0, size: 64 };
+
 const validator = z.object({
-  radius: z.number().int().min(0).default(0),
-  size: z.number().int().min(1).default(64),
+  radius: z.number().int().min(0).default(defaults.radius),
+  size: z.number().int().min(1).default(defaults.size),
 });
 
 type Options = z.input<typeof validator>;
@@ -14,5 +16,5 @@ function validate(opts: Options): Options {
   return validator.parse(opts);
 }
 
-export { validate };
 export type { Options };
+export { defaults, validate };
